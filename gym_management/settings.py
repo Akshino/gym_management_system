3e27eec72b8524a7612e25a8383ef2e5f49e43bd
Akshino <144610952+Akshino@users.noreply.github.com>
2025-03-24 13:08:38 +0530

    Update settings.py

--- gym_management/settings.py
+++ gym_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-(!*%9m2vg5&*3r7zcmn&uhl9s21w-ca837$dt_0@4rryb+(a39
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
